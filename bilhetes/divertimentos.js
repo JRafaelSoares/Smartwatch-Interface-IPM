19338140362bfcfc4aa57a41dffc7b4d9d6e38c0
JRafaelSoares <j.rafael.soares.98@gmail.com>
2018-04-15 23:39:38 +0100

    Organized functionalities
    "Bilhetes" Completed, make popups go back

---
+++ bilhetes/divertimentos.js
@@ -0,0 +1,35 @@
+/* exported getDivertimentos */
+/* exported getTickets */
+/* exported setTickets */
+/* exported getCancelarBilhete */
+/* exported setStuffs */
+
+var diver = { divertimentos: [
+        { "nome": "Montanha Russa", "hora": "11:45", "tirado": 0},
+        { "nome": "Carrosel", "hora": "15:45", tirado: 0},
+        { "nome": "Roda Gigante", "hora": "16:45", tirado: 0}]};
+
+var cancelarBilhete = { onHold: []};
+
+function getDivertimentos() {
+    "use strict";
+    return diver.divertimentos;
+}
+
+function getTickets() {
+    "use strict";
+    return tickets.bilhetes;
+}
+
+function getCancelarBilhete(){
+    return cancelarBilhete.onHold;
+}
+
+
+function setTickets(newTickets){
+    tickets.bilhetes.push(newTickets);
+}
+
+function setStuffs(newCenas) {
+    cancelarBilhete.onHold[0]=newCenas;
+}
