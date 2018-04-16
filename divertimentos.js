313fc832ed83f621ae18ed6eb6dc4623adcfb508
JRafaelSoares <j.rafael.soares.98@gmail.com>
2018-04-16 11:38:18 +0100

    Added new secondary screen for "Friends"

--- divertimentos.js
+++ divertimentos.js
@@ -7,7 +7,8 @@
 var diver = { divertimentos: [
         { "nome": "Montanha Russa", "hora": "11:45", "tirado": 0},
         { "nome": "Carrosel", "hora": "15:45", tirado: 0},
-        { "nome": "Roda Gigante", "hora": "16:45", tirado: 0}]};
+        { "nome": "Roda Gigante", "hora": "16:45", tirado: 0},
+        { "nome": "Rapel", "hora": "23:50", tirado: 0}]};
 
 var cancelarBilhete = { onHold: []};
 
