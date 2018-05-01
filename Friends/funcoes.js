f1845369ef2eefd3ad02f99ea14be74d5d2de38a
JRafaelSoares <j.rafael.soares.98@gmail.com>
2018-05-01 21:18:06 +0100

    Favorite Issues done

--- Friends/funcoes.js
+++ Friends/funcoes.js
@@ -1,8 +1,3 @@
-/* exported loadAllRoomData */
-/* exported loadAllBilhetes */
-/* exported displayBilhetes */
-/* exported loadAllStuff */
-
 function loadOnDisplay(onDisplay) {
     "use strict";
     if( localStorage.getItem("Display") == null){
@@ -16,7 +11,35 @@ function loadAllFriends(allFriends) {
         localStorage.setItem("Friends", JSON.stringify(allFriends));
     }
 }
+function toggleFavorite() {
+    var amigosStr = localStorage.getItem("Friends");
+    
+    var amigos = JSON.parse(amigosStr);
+    
+    var displayStr = localStorage.getItem("Display");
+    
+    var display = JSON.parse(displayStr);
+    
+    var i=0;
 
+    var tam = amigos.length;
+    
+    var nome = document.getElementById("nome");
+    
+    for(i; i<tam; i++){
+        if(amigos[i].nome == display[0].nome && amigos[i].surname == display[0].surname){
+            if(amigos[i].favorito == 0){
+                amigos[i].favorito = 1;
+                nome.innerHTML = '<span class="text" align="center">' +display[0].nome + "." + display[0].surname.charAt(0) + "<img src='icones-pretos/favorite_blue.png' id= 'favorite'></span>";
+            }
+            else{
+                amigos[i].favorito = 0;
+                nome.innerHTML = '<span class="text" align="center">' +display[0].nome + "." + display[0].surname.charAt(0) + '<img src="icones-pretos/unfavorite.png" id="favorite"></span>';
+            }
+        }
+    }
+    localStorage.setItem("Friends", JSON.stringify(amigos));
+}
 function displayFriends() {
     "use strict";
     
@@ -32,10 +55,10 @@ function displayFriends() {
         if(amigos[i].added == 1){
             j++;
             if(amigos[i].favorito == 0){
-                lista.innerHTML += '<div class="entry" onclick="addOnHoldFriend(' +i +'); window.location.href = \'Friend.html\';"> <span class="text" align="center">' + amigos[i].nome + "." + amigos[i].surname.charAt(0) + '</span> </div>';
+                lista.innerHTML += '<div class="entry" onclick="addOnHoldFriend(' +i +'); window.location.href = \'Friend.html\';"> <span class="text" align="center">' + amigos[i].nome + "." + amigos[i].surname.charAt(0) + '</span>  </div>';
             }
             else{
-                lista.innerHTML += '<div class="entry" onclick="addOnHoldFriend(' +i +'); window.location.href = \'friend_favorite.html\';"> <span class="text" align="center">' + amigos[i].nome + "." + amigos[i].surname.charAt(0) + '</span> </div>';
+                lista.innerHTML += '<div class="entry" onclick="addOnHoldFriend(' +i +'); window.location.href = \'friend_favorite.html\';"> <span class="text" align="center">' + amigos[i].nome + "." + amigos[i].surname.charAt(0) + '</span> <img src="icones-pretos/favorite_blue.png" id="favorite"></div>';
             }
         }
     }
@@ -59,8 +82,7 @@ function displayFavorites(){
     for (i; i < amigos.length; i++) {
         if(amigos[i].favorito == 1){
             empty = 1;
-            console.log("ADICIONEI");
-            lista.innerHTML += '<div class="entry" onclick="addOnHoldFriend('+i+'); window.location.href = \'friend_favorite.html\';"> <span class="text" align="center">'+amigos[i].nome+'</span></div>';
+            lista.innerHTML += '<div class="entry" onclick="addOnHoldFriend('+i+'); window.location.href = \'friend_favorite.html\';"> <span class="text" align="center">'+amigos[i].nome+ "." + amigos[i].surname.charAt(0) + '</span></div>';
         }
     }
     if(empty == 0){
@@ -82,13 +104,28 @@ function addOnHoldFriend(i) {
 }
 
 function loadFriend() {
+    var amigosStr = localStorage.getItem("Friends");
+    
+    var amigos = JSON.parse(amigosStr);
+    
     var friendStr = localStorage.getItem("Display");
     
     var friend = JSON.parse(friendStr);
     
     var name = document.getElementById("nome");
     
-    name.innerHTML = '<span class="text" align="center">' +friend[0].nome + '</span>';
+    var i =0;
+    for(i; i<amigos.length; i++)
+        if(amigos[i].nome == friend[0].nome && amigos[i].surname == friend[0].surname){
+            if(amigos[i].favorito == 0){
+                console.log("poop");
+                name.innerHTML = '<span class="text" align="center">' +friend[0].nome + "." + friend[0].surname.charAt(0) + "<img src='icones-pretos/unfavorite.png' id= 'favorite'></span>";
+            }
+            else{
+                console.log("notpoop");
+                name.innerHTML = '<span class="text" align="center">' +friend[0].nome + "." + friend[0].surname.charAt(0) + '<img src="icones-pretos/favorite_blue.png" id="favorite"></span>';}
+        }
+           
 }
 
 function loadFindFriend() {
@@ -98,7 +135,7 @@ function loadFindFriend() {
     
     var name = document.getElementById("nome");
     
-    name.innerHTML = '<span class="text" align="center">' +friend[0].nome + ' ' + friend[0].km + 'km</span>';
+    name.innerHTML = '<span class="text" align="center">' +friend[0].nome + '.' + friend[0].surname.charAt(0) + " " + friend[0].km + 'km</span>';
 }
 
 function addFavorite() {
@@ -123,7 +160,6 @@ function addFavorite() {
         }
     }
     
-    console.log(friends[0].favorito);
     localStorage.setItem("Friends", JSON.stringify(friends));
 }
 
